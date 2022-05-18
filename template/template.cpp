a2f69ddfbbdccd1698b5e09c549d9056e2a09ad7
depgit <depcoder@gmail.com>
2022-05-19 00:10:07 +0530

    now

--- template/template.cpp
+++ template/template.cpp
@@ -35,4 +35,7 @@ using namespace std;
 using namespace __gnu_pbds;
 template <typename T> using indexed_multiset = 
     tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+function<bool(int)> dfs = [&](int u)->bool{
     
+};
