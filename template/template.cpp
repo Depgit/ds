b4351b283b354d4b0eb0c8b4cffd8f3e0d7c5739
depgit <depcoder@gmail.com>
2022-08-01 17:33:13 +0530

    good

--- template/template.cpp
+++ template/template.cpp
@@ -41,6 +41,35 @@ function<bool(int)> dfs = [&](int u)->bool{
     
 };
 
+multiset<ll> LIS(vector<ll> A)
+{
+	ll a = A.size();
+	multiset<ll> S;
+	multiset<ll>::iterator it;
+	rep(i,a)
+	{
+		S.insert(A[i]);
+		it = S.upper_bound(A[i]);
+		if (it != S.end()) S.erase(it);
+	}
+	return S;
+}
+ 
+multiset<ll> LSIS(vector<ll> A) // a.k.a Longest Strictly Increasing Sequence
+{
+	ll a = A.size();
+	multiset<ll> S;
+	multiset<ll>::iterator it;
+	rep(i,a)
+	{
+		S.insert(A[i]);
+		it = S.lower_bound(A[i]);
+		it++;
+		if (it != S.end()) S.erase(it);
+	}
+	return S;
+}
+
 // set comparator
 struct comp{
     bool operator()(const vl &a,const vl &b){
